5749547378f3b0ad66c2380d5d326f7da969d8c7
AKAAKAI <twnoken@gmail.com>
2017-03-07 03:20:38 +0800

    update hw3

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -4,8 +4,24 @@ module LuhnValidator
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
   def validate_checksum
-    nums_a = number.to_s.chars.map(&:to_i)
-
+    nums_a = number.to_s.chars.map(&:to_i).reverse!
     # TODO: use the integers in nums_a to validate its last check digit
+    total_num = 0
+    nums_a.length.times do |x| 
+    	if x.even? 
+    		
+    		total_num += nums_a[x]
+    	else 
+
+    		if (nums_a[x]*2) >= 10
+				tem = (nums_a[x]*2).to_s.chars.map(&:to_i) 
+    			tem.length.times { |q| total_num += tem[q] }
+    		else
+    			total_num += nums_a[x]*2 
+    		end
+    	end
+    end
+
+    return total_num % 10 == 0 ? true : false
   end
 end
